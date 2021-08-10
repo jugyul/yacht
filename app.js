56120d5a54ed740b56866a8969bbd5f0758136d3
jugyul <88657977+jugyul@users.noreply.github.com>
2021-08-10 14:31:24 +0000

    plus id & use for()

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const rollingBtn = document.getElementById("rollingBtn")
 function onRollingBtnClick(){
   for (let i =0 ; i<5 ;i++){
     const value = Math.floor(Math.random()*6+1)
-    randomValue.push(value)
+    const randomDice = document.getElementById(`d${i}`);
+    randomDice.innerText = value;
   }
 }
 
