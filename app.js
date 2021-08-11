d65f5cd13b80c800a6af672a26b88054f7127830
jugyul <88657977+jugyul@users.noreply.github.com>
2021-08-11 10:43:20 +0000

    random/saved Dice funtion

--- app.js
+++ app.js
@@ -1,12 +1,58 @@
-const randomValue = []
-const rollingBtn = document.getElementById("rollingBtn")
+const rollBtn = document.getElementById("rollBtn")
+const savedDiceValues =[]
 
-function onRollingBtnClick(){
-  for (let i =0 ; i<5 ;i++){
-    const value = Math.floor(Math.random()*6+1)
-    const randomDice = document.getElementById(`d${i}`);
-    randomDice.innerText = value;
-  }
+function createRandomDice(value, newTryOfRolling){
+  const randomDice = document.createElement("div")
+  randomDice.classList.add("randomDice")
+  randomDice.innerText = value;
+  newTryOfRolling.appendChild(randomDice)
+  randomDice.addEventListener("click", onRanDiceClick)  
 }
 
-rollingBtn.addEventListener("click", onRollingBtnClick)
+function onSavedDiceClick(event){
+  const clickedDice = event.target
+  const value = clickedDice.innerText
+  const newTryOfRolling = document.querySelector("#dices .randomDices .trialOfRolling")
+  createRandomDice(value, newTryOfRolling)
+  clickedDice.remove()
+}
+
+function onRanDiceClick(event){
+  const clickedDice = event.target
+  const value = clickedDice.innerText //saved를 만들기
+  savedDiceValues.push(value)
+  const savedDices = document.querySelector("#dices .savedDices")
+  const savedDice = document.createElement("div")
+  savedDice.classList.add("savedDice")
+  savedDice.innerText=value
+  savedDices.appendChild(savedDice)
+  savedDice.addEventListener("click", onSavedDiceClick)
+  clickedDice.remove()
+}
+
+function resetDices(){
+  const randomDices = document.querySelector("#dices .randomDices")
+  const tryOfRolling = randomDices.querySelector(".trialOfRolling")
+  tryOfRolling.remove()
+  const newTryOfRolling = document.createElement("div")
+  newTryOfRolling.classList.add("trialOfRolling")
+  randomDices.appendChild(newTryOfRolling)
+  return newTryOfRolling
+}
+
+
+
+function onRollBtnClick(){
+  const newTryOfRolling = resetDices() //가독성 ??
+
+  for (let i =0 ; i<5 ;i++){
+    const value = Math.floor(Math.random()*6+1)
+    createRandomDice(value, newTryOfRolling)
+  }
+
+
+}
+
+
+rollBtn.addEventListener("click", onRollBtnClick)
+
