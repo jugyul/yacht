0fdae0ae4b1b5142e93d6df8911771f91a79a1ad
jugyul <88657977+jugyul@users.noreply.github.com>
2021-08-12 11:14:10 +0000

    dice

--- dice.js
+++ dice.js
@@ -1,34 +1,44 @@
 const rollBtn = document.getElementById("rollBtn")
-const savedDiceValues =[]
+let savedDiceValues =[]
+let rollBtnClickCount=0
+
 
 function createRandomDice(value, newTryOfRolling){
   const randomDice = document.createElement("div")
   randomDice.classList.add("randomDice")
-  randomDice.innerText = value;
+  randomDice.classList.add("clicked")
+  randomDice.innerText = value
+  randomDice.id=Date.now()
   newTryOfRolling.appendChild(randomDice)
-  randomDice.addEventListener("click", onRanDiceClick)  
+  randomDice.addEventListener("click", moveToSaved)  
 }
 
-function onSavedDiceClick(event){
+
+
+function moveToRandom(event){
   const clickedDice = event.target
   const value = clickedDice.innerText
   const newTryOfRolling = document.querySelector("#dices .randomDices .trialOfRolling")
   createRandomDice(value, newTryOfRolling)
+  savedDiceValues = savedDiceValues.filter((value)=>value.id !== clickedDice.id)
   clickedDice.remove()
 }
 
-function onRanDiceClick(event){
+function moveToSaved(event){
   const clickedDice = event.target
-  const value = clickedDice.innerText //saved를 만들기
-  savedDiceValues.push(value)
   const savedDices = document.querySelector("#dices .savedDices")
   const savedDice = document.createElement("div")
   savedDice.classList.add("savedDice")
-  savedDice.innerText=value
+  savedDice.classList.add("clicked")
+  savedDice.id=clickedDice.id
+  savedDice.innerText=clickedDice.innerText
   savedDices.appendChild(savedDice)
-  savedDice.addEventListener("click", onSavedDiceClick)
+  saveValue(clickedDice)
+  savedDice.addEventListener("click", moveToRandom)
   clickedDice.remove()
 }
+
+
 
 function resetDices(){
   const randomDices = document.querySelector("#dices .randomDices")
@@ -41,18 +51,36 @@ function resetDices(){
 }
 
 
+function rolling (newTryOfRolling){
+  const value = Math.floor(Math.random()*6+1)
+  createRandomDice(value, newTryOfRolling)
+}
+
+function stopRolling(interval){
+  clearInterval(interval)
+}
+
+function saveValue(element){
+  const valueObj={}
+  valueObj.value = element.innerText
+  valueObj.id=element.id
+  savedDiceValues.push(valueObj)
+}
 
 function onRollBtnClick(){
   const newTryOfRolling = resetDices() //가독성 ??
+  const interval = setInterval(rolling,100,newTryOfRolling)
+  setTimeout(stopRolling,510-savedDiceValues.length*100,interval)
 
-  for (let i =0 ; i<5 ;i++){
-    const value = Math.floor(Math.random()*6+1)
-    createRandomDice(value, newTryOfRolling)
+  rollBtnClickCount++
+  if( rollBtnClickCount===3){
+    rollBtn.classList.add("hidden")
+  
   }
-
 
 }
 
 
-rollBtn.addEventListener("click", onRollBtnClick)
 
+
+rollBtn.addEventListener("click", onRollBtnClick)
